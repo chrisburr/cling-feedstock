4081780b5c52a2c235185c2c35e61ffe4d1bd85b
Chris Burr <christopher.burr@cern.ch>
2021-03-13 14:09:49 +0100

    First half-functional build of ROOT with LLVM 9

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,13 @@ set -x
 mkdir build
 cd build
 
+# Cling needs some minor patches to the LLVM sources
+sed -i "s@LLVM_LINK_LLVM_DYLIB yes@LLVM_LINK_LLVM_DYLIB no@g" "${PREFIX}/lib/cmake/llvm/LLVMConfig.cmake"
+cd "${PREFIX}"
+patch -p1 < "${RECIPE_DIR}/llvm-patches/0001-Fix-the-compilation.patch"
+patch -p1 < "${RECIPE_DIR}/llvm-patches/0002-Make-datamember-protected.patch"
+cd -
+
 cmake \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
@@ -12,9 +19,10 @@ cmake \
   -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
   -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
   -DCMAKE_BUILD_TYPE=Release \
+  -DLLVM_CXX_STD=c++17 \
   ..
 
 make -j${CPU_COUNT}
 make install
 
-patch -d $PREFIX -R -p1 -i $RECIPE_DIR/llvm-headers.patch
+# patch -d $PREFIX -R -p1 -i $RECIPE_DIR/llvm-headers.patch
